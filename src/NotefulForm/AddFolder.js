bb6a21c38e807aabc9f45a1a18a044b8791f6db6
aniledev <elinamcgill@gmail.com>
2021-01-22 04:29:20 -0500

    REFACTOR: change AddFolder component into class component

--- src/NotefulForm/AddFolder.js
+++ src/NotefulForm/AddFolder.js
@@ -1,30 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import "./AddFolder.css";
 
-export default function AddFolder(props) {
-  const { className, ...otherProps } = props;
-  return (
-    <form
-      className={["AddFolder", className].join(" ")}
-      action="#"
-      {...otherProps}
-    >
-      <h2>Add a new folder</h2>
+export default class AddFolder extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-      <div className="form-group">
-        <label>Folder Name *</label>
-        <input
-          type="text"
-          className="name-input"
-          name="name"
-          id="name"
-          defaultValue="New Folder"
-        />
-      </div>
-      <button type="submit" className="name-button">
-        Add Folder
-      </button>
-      <div className="input-hint">* required field</div>
-    </form>
-  );
+  render() {
+    return (
+      <form className="AddFolder" action="#">
+        <h2>Add a new folder</h2>
+
+        <div className="form-group">
+          <label>Folder Name *</label>
+          <input
+            type="text"
+            className="name-input"
+            name="name"
+            id="name"
+            defaultValue="New Folder"
+            onChange={e => this.updateFolderName(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="name-button">
+          Add Folder
+        </button>
+        <div className="input-hint">* required field</div>
+      </form>
+    );
+  }
 }
