6e619a50bf998c4aad5fbf9a64d44e81762bceb0
aniledev <elinamcgill@gmail.com>
2021-01-22 06:15:35 -0500

    FEAT: add create context with empty updater functions

---
+++ apiContext.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// use createContext to initialize context for use in application
+
+export default React.createContext({
+  notes: [],
+  folders: [],
+  // empty update functions
+  addFolder: () => {},
+  addNote: () => {},
+  deleteNote: () => {}
+});
