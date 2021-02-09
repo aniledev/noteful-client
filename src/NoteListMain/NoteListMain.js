7d8dec3b9a833cdbf4838b4435d7bd5fcac24c63
aniledev <elinamcgill@gmail.com>
2021-02-09 00:32:38 -0500

    FEAT: update helper functions so filter method works correctly

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -18,9 +18,11 @@ export default class NoteListMain extends React.Component {
 
   render() {
     const { folderId } = this.props.match.params;
-    const { notes = [] } = this.context;
+    const { notes } = this.context;
     const notesForFolder = getNotesForFolder(notes, folderId);
     console.log(folderId);
+    console.log(notes);
+
     return (
       <section className="NoteListMain">
         <ul>
--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -5,7 +5,7 @@ export const findNote = (notes = [], noteId) =>
   notes.find(note => note.id === noteId);
 
 export const getNotesForFolder = (notes = [], folderId) =>
-  !folderId ? notes : notes.filter(note => note.folderId === folderId);
+  !folderId ? notes : notes.filter(note => note.folderid == folderId);
 
 export const countNotesForFolder = (notes = [], folderId) =>
   notes.filter(note => note.id === folderId).length;
